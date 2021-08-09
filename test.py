310ed868c72e22a187930f4d34afe50967cd1094
hungbk59 <84137783+hungbk59@users.noreply.github.com>
2021-08-09 16:16:18 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import peewee
 from peewee import MySQLDatabase
-#sử dung peewee bị lỗi không tao dược database ban đầu
-db = MySQLDatabase ('scarpe', user='admin', password='abc123')
+#tạo database
+db = MySQLDatabase ('test', user='admin', password='abc123')
 mycursor = db.cursor()
 #xóa table database
 try:
@@ -14,7 +14,8 @@ except:
 
 link_base = "https://vnexpress.net/giao-duc"
 list_link = []
-page = 3 #số (n-1) trang muốn lấy thông tin
+print('nhập số lượng page:')
+page = int(input()) #số (n-1) trang muốn lấy thông tin
 for num in range(1,page):
     list_link.append(link_base + '-p' + str(num))
 for i in range(len(list_link)):
@@ -30,51 +31,22 @@ for i in range(len(list_link)):
         try:
             title = soup.find("h1", class_="title-detail").text.strip()
             decript = soup.find("p", class_="description").text.strip()
-            # chỉ lấy được nọi dung đầu tiên trong <article ...<p class='Normal'....</article>
-            contens = soup.find("p", class_="Normal").text
+            body = soup.find_all('p', class_='Normal')
+            contents =''
+            for content in body:
+                contents += ' ' + content.text
         except:
             title = ""
             decript = ""
-            contens = ""
-        print("Tiêu đề: " + title)
-        print("Mô tả: " + decript)
-        print("Nội dung:" + contens)
-        print("_________________________________________________________________________")
+            contents = ""
 
         class data(peewee.Model):
             tieude = peewee.TextField()
+            mota = peewee.TextField()
             noidung = peewee.TextField()
 
             class Meta:
                 database = db
         data.create_table()
-        Data = data(tieude=title, noidung=decript)
+        Data = data(tieude=title, mota=decript, noidung=contents)
         Data.save()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
