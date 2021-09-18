3904a977b55ab59c0962344563a39d3efeaecf2d
hungbk59 <84137783+hungbk59@users.noreply.github.com>
2021-09-18 15:59:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,32 @@ import models
 import schedule
 import time
 from bs4 import BeautifulSoup
+from multiprocessing import Process
 
-def crwal():
-    link_base = "https://vnexpress.net/giao-duc"
-    check_page = ""
+# Hàm lấy ra danh sách link page
+def get_link(link_base):
+    list_link = []
+    link_page = link_base
+    csc = 0  # Check status code
+    num = 0  # Number page
+    while csc != 302:
+        num += 1
+        list_link.append(link_page)
+        link_page = link_base + '-p' + str(num)
+        r = requests.get(link_page)
+        check_redirect = r.history
 
-    while check_page != "javascript:;":
+        for stc in check_redirect:
+            csc = stc.status_code
+    return list_link
 
-        r = requests.get(link_base)
+# Hàm cào dữ liệu link bài báo, lưu và check dữ liệu trong database
+def crwal(list_link):
+
+    for L in range(len(list_link)):
+        link_page = list_link[L]
+        r = requests.get(link_page)
+        r = requests.get(link_page)
         soup = BeautifulSoup(r.content, "html.parser")
         titles = soup.findAll('h3', class_='title-news')
         links = [link.find('a').attrs["href"] for link in titles]
@@ -32,25 +50,23 @@ def crwal():
                 contents += ' ' + content.text
 
             data = models.database()
-            check_data = data.select(data.mota).where(data.mota == decript).count()
-            # Kiểm tra sự tồn tại của bài báo
-            if check_data == 0:
-                query = data.insert(tieude=title, mota=decript, noidung=contents).execute()
-            elif check_data >0:
+            check_link = data.select(data.https).where(data.https == link).count()
+
+            # Kiểm tra sự trung lặp của link
+            if check_link == 0:
+                query = data.insert(tieude=title, mota=decript, noidung=contents, https=link).execute()
+            else:
                 break
 
-        # Kiểm tra các link page tiếp theo
-        link_next = soup.findAll('a', class_='btn-page next-page')
-        link_end = soup.findAll('a', class_='btn-page next-page disable')
-        for link_end_tail in link_end:
-            check_page = link_end_tail.get('href')
+def main():
+    p = Process(target=crwal, args=(get_link('https://vnexpress.net/giao-duc'),))
+    p.start()
+    p.join()
+def run ():
+    if __name__ == "__main__":
+        main()
 
-        # Lấy ra href của trang tiếp theo
-        for link_tail in link_next:
-            link_page = "https://vnexpress.net" + link_tail.get('href')
-            link_base = link_page
-
-schedule.every().day.at("17:00").do(crwal)
+schedule.every().day.at("17:00").do(run)
 
 # Kiểm tra schedule
 while True:
