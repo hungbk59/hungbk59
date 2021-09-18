24afe59275e693f0d1380b022b706f9e27b727fc
hungbk59 <84137783+hungbk59@users.noreply.github.com>
2021-09-18 16:01:02 +0700

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,18 @@
-from peewee import (MySQLDatabase, Model, TextField, AutoField)
+from peewee import (MySQLDatabase, Model, TextField, AutoField, CharField)
 
 db = MySQLDatabase('test', user='admin', password='abc123')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class data(BaseModel):
+    STT = AutoField()
+    tieude = CharField()
+    mota = TextField()
+    noidung = TextField()
+    https = CharField()
+
 def database():
-    class data(Model):
-        STT = AutoField()
-        tieude = TextField()
-        mota = TextField()
-        noidung = TextField()
-
-        class Meta:
-            database = db
-            db_table = 'data'
-
     data.create_table()
     return data
