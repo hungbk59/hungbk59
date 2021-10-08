8187cfb3de9daf3db18fc1e154efaaf843e37f73
hungbk59 <84137783+hungbk59@users.noreply.github.com>
2021-10-08 16:19:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,78 +1,13 @@
-import requests
 import models
-from bs4 import BeautifulSoup
-from multiprocessing import Process, Queue
-
 from flask import Flask, jsonify, request, session
 from werkzeug.security import generate_password_hash
 
-
+app = Flask(__name__)
 data = models.Data
 user = models.Users
-app = Flask(__name__)
 app.secret_key = generate_password_hash('Herocoders')
 
 
-def get_link(links_base, queue):
-    csc = 0  # Check status code
-    num = 0  # Number page
-    while csc != 302:
-        num += 1
-        link_page = links_base + '-p' + str(num)
-        r = requests.get(link_page, allow_redirects=False)
-        csc = r.status_code
-        queue.put(link_page)
-
-    queue.put("KETTHUC")
-    queue.put("KETTHUC")
-    queue.put("KETTHUC")
-    queue.put("KETTHUC")
-
-
-def scrape(queue):
-    while True:
-        link_page = queue.get()
-        if link_page == "KETTHUC":
-            break
-        print(link_page)
-        r = requests.get(link_page)
-        soup = BeautifulSoup(r.content, "html.parser")
-        titles = soup.findAll('h3', class_='title-news')
-        links = [link.find('a').attrs["href"] for link in titles]
-
-        for link in links:
-            news = requests.get(link)
-            soup_news = BeautifulSoup(news.content, "html.parser")
-            contents = ""
-            try:
-                title = soup_news.find("h1", class_="title-detail").text.strip()
-                decript = soup_news.find("p", class_="description").text.strip()
-                body = soup_news.find_all('p', class_='Normal')
-            except soup_news:
-                title = ""
-                decript = ""
-                body = ""
-
-            for content in body:
-                contents += ' ' + content.text
-
-            check_link = data.select(data.uri).where(data.uri == link).count()
-
-            # Kiểm tra sự trung lặp của link
-            if check_link == 0:
-                data.insert(tieude=title, mota=decript, noidung=contents, uri=link).execute()
-            else:
-                break
-
-
-def main(queue):
-    for i in range(1, 5):
-        pi_crawl = Process(target=scrape, args=(queue,))
-        pi_crawl.start()
-    for i in range(1, 5):
-        pi_crawl.join()
-
-        
 def check_character(character):
     """Danh sách các ký tự không mong muốn"""
     special = '''`~!@#$%^&*()_-=+-*/\[]{}|:;<>'"?'''
@@ -82,53 +17,61 @@ def check_character(character):
     return 1
 
 
+def loc(char):
+    """Danh sách các ký tự loại bỏ"""
+    special = '''`~!@#$%^&*()_-=+-*/\[]{}|:;<>'"?'''
+    for i in range(len(special)):
+        char = char.replace(special[i], '')
+    return char
+
+
 def filters(char):
+    """Kiểm tra từng phần tử trong chuỗi"""
     for i in range(len(char)):
         if check_character(char[i]) == 0:
             return 0
     return 1
 
 
-@app.route("/data/search&page=<int:ph>&limit=<int:pe>", methods=["GET"])
-def get_search(ph, pe):
-    input_title = request.form["tieude"]
-    input_content = request.form["noidung"]
-    # Tìm kiếm dữ liệu theo tiêu đề và nội dung bài viết
-    search_title = "%" + input_title + "%"
-    search_content = "%" + input_content + "%"
+@app.route("/data/search", methods=["GET"])
+def get_search():
+    """Tìm kiếm bài báo theo tiêu đề, nội dung với kết quả tìm kiếm được giới hạn""" 
+    tieude = request.args.get("tieude", type=str)
+    noidung = request.args.get("noidung", type=str)
+    page = request.args.get("page", type=int)
+    limit = request.args.get("limit", type=int)
 
-    # Kiểm tra dữ liệu nhập vào có hợp lệ
-    if filters(input_title) is False or filters(input_content) is False:
-        return jsonify({"status": -2, "message": "Error input data"})
+    if filters(tieude) == 0 or filters(noidung) == 0:
+        tieude = loc(tieude)
+        noidung = loc(noidung)
 
-    if request.form.get("tieude") and request.form.get("noidung"):
-        new = (data
-               .select()
-               .where(data.tieude ** search_title, data.noidung ** search_content)
-               .limit(pe).offset(ph)
-               .dicts())
-        news = []
+    new = (data
+           .select()
+           .where(data.tieude ** ("%" + tieude + "%"), data.noidung ** ("%" + noidung + "%"))
+           .limit(limit).offset(page)
+           .dicts())
+    news = []
 
-        for row in new:
-            news.append(row)
-        return jsonify({"list_news": news})
-    return jsonify({"status": -1, "message": "Need add tieude or noidung"})
+    for row in new:
+        news.append(row)
+    return jsonify({"list_news": news})
 
 
 @app.route("/data/update", methods=["PUT", "POST"])
 def update_data():
+    # Kiểm tra trang thái login
     if 'email' in session:
-        input_title = request.form["tieude"]
-        input_content = request.form["noidung"]
+        _title = request.form["tieude"]
+        _content = request.form["noidung"]
 
         # Kiểm tra dữ liệu nhập vào có hợp lệ
-        if filters(input_title) is False or filters(input_content) is False:
-            return jsonify({"status": -2, "message": "Error input data"})
+        if filters(_title) == 0 or filters(_content) == 0:
+            return jsonify({"status": -2, "message": "Re-enter Data"})
 
         # Sửa tiêu đề và nội dung theo URI của bài báo
         if request.method == "PUT":
             (data
-             .update(tieude=request.form["tieude"], noidung=request.form["noidung"])
+             .update(tieude=_title, noidung=_content)
              .where(data.uri == request.form["uri"]).execute())
 
             return jsonify({"status": 1, "message": "Update Successfull"})
@@ -136,7 +79,7 @@ def update_data():
         # Thêm tiêu đề và nội dung của 1 bài báo mới
         elif request.method == "POST":
             (data
-             .insert(tieude=request.form["tieude"], noidung=request.form["noidung"])
+             .insert(tieude=_title, noidung=_content)
              .execute())
 
             return jsonify({"status": 1, "message": "Insert Successfull"})
@@ -148,6 +91,7 @@ def update_data():
 
 @app.route("/data/delete%<int:new_id>", methods=["DELETE"])
 def delete_data():
+    # Kiểm tra trang thái login
     if 'email' in session:
         data.delete().where(data.uri == request.form["uri"]).execute()
         return jsonify({"status": 1, "message": "Delete Successfull"})
@@ -159,6 +103,7 @@ def delete_data():
 
 @app.route("/login", methods=["POST"])
 def login():
+    """Đăng nhập email/password và xác thực email/password đó"""
     _email = request.form["email"]
     _password = request.form["password"]
     if request.form.get("email") and request.form.get("password"):
@@ -203,12 +148,5 @@ def handl_500_error(_error):
     return jsonify({"status": 500, "message": "Server error"})
 
 
-if __name__ == "__main__":
-    link_base = 'https://vnexpress.net/giao-duc'
-    q = Queue()
-    p_getlink = Process(target=get_link, args=(link_base, q,))
-    p_getlink.start()
-    main(q)
-    p_getlink.join()
-    
+if __name__ == '__main__':
     app.run(debug=True)
